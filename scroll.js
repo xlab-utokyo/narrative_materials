81ff941875c6b734dd6368293cdd7a8486d3dbbd
Maku459 <hzk.butterfly459@gmail.com>
2021-02-18 14:26:17 +0900

    first commit

---
+++ scroll.js
@@ -0,0 +1,48 @@
+// スムーススクロール
+const paginations = document.querySelectorAll(".pagination a");
+paginations.forEach(pagination => {
+  pagination.addEventListener("click", e => {
+    e.preventDefault();
+    const targetId = e.target.hash;
+    const target = document.querySelector(targetId);
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+// Intersection Observer
+const sections = document.querySelectorAll(".section");
+const observerRoot = document.querySelector(".fullPageScroll");
+const options = {
+  root: observerRoot,
+  rootMargin: "-50% 0px",
+  threshold: 0
+};
+const observer = new IntersectionObserver(doWhenIntersect, options);
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+/**
+ * 交差したときに呼び出す関数
+ * @param entries - IntersectionObserverEntry IntersectionObserverが交差したときに渡されるオブジェクトです。
+ */
+function doWhenIntersect(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      activatePagination(entry.target);
+    }
+  });
+}
+
+/**
+ * ページネーションの大きさを変える関数
+ * @param element - HTMLElement 現在表示中のスライドのHTML要素を引数に取ります。
+ */
+function activatePagination(element) {
+  const currentActiveIndex = document.querySelector("#pagination .active");
+  if (currentActiveIndex !== null) {
+    currentActiveIndex.classList.remove("active");
+  }
+  const newActiveIndex = document.querySelector(`a[href='#${element.id}']`);
+  newActiveIndex.classList.add("active");
+}
