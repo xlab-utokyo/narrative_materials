f246b39e0d9ca2cb6527fbc3908a59db6d95941e
Maku459 <hzk.butterfly459@gmail.com>
2021-02-25 15:05:36 +0900

    change design

---
+++ imgscroll.js
@@ -0,0 +1,28 @@
+const SIZE = 25; // 枚数
+var tmp = {};
+    function loadImageToTmp(){
+        for(var i=1;i<=SIZE;i++){
+            const _i = i;
+            const img = new Image();
+            tmp[_i] = null;
+            img.src = "anim/img ("+_i+").png"; // 連続するファイル名
+            img.addEventListener("load",()=>{
+                tmp[_i] = img;
+            })
+        }
+    }
+
+    const PX = 5; // 5px分の移動ごと画像を1枚進める   
+    const offset = $("#anim_img_box").offset(); // 画像を入れるdiv要素(position:stickyでトップに来たら固定される)
+        $(window).scroll(function() {
+            const y = $(window).scrollTop();
+            const dy = y - offset.top;
+            if(offset.top<y&&y<offset.top+SIZE*PX){
+                $("#anim_img_box").css("top",0)
+                const i = Math.floor(dy / PX);
+                if(i<=0||i>=SIZE) return;
+                if(tmp[i].src) image.src = tmp[i].src;
+            }else if(y>=offset.top+SIZE*PX){
+                $("#anim_img_box").css("top","-"+(dy-SIZE*PX)); // スクロール分が終了したときに移動を始める
+            }
+        }); 
